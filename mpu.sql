e6ff460eb2231a6b95ed34a7c8865c74f076554c
Thopo Martha Akbar <thopo.martha@gmail.com>
2017-12-29 09:54:46 +0700

    update sql

--- mpu.sql
+++ mpu.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2017 at 01:00 AM
+-- Generation Time: Dec 29, 2017 at 03:53 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.5.38
 
@@ -31,9 +31,10 @@ CREATE TABLE `chip_code` (
   `code` varchar(100) NOT NULL,
   `type` varchar(10) NOT NULL,
   `description` varchar(300) DEFAULT NULL,
+  `description_cn` varchar(300) CHARACTER SET gbk DEFAULT NULL,
   `is_deleted` varchar(1) NOT NULL DEFAULT 'N',
-  `created_by` varchar(20) NOT NULL,
-  `updated_by` varchar(20) NOT NULL,
+  `created_by` varchar(20) NOT NULL DEFAULT 'SYSTEM',
+  `updated_by` varchar(20) NOT NULL DEFAULT 'SYSTEM',
   `created_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -46,42 +47,14 @@ CREATE TABLE `chip_code` (
 
 CREATE TABLE `gallery` (
   `id` int(10) UNSIGNED ZEROFILL NOT NULL,
+  `code` varchar(100) NOT NULL,
   `filename` varchar(30) NOT NULL,
-  `type` varchar(10) NOT NULL,
-  `description` varchar(200) DEFAULT NULL,
-  `description_cn` varchar(200) CHARACTER SET gbk DEFAULT NULL,
   `is_deleted` varchar(1) NOT NULL DEFAULT 'N',
   `created_by` varchar(20) NOT NULL,
   `updated_by` varchar(20) NOT NULL,
   `created_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `main_header`
---
-
-CREATE TABLE `main_header` (
-  `id` int(10) UNSIGNED ZEROFILL NOT NULL,
-  `small_text` varchar(200) NOT NULL,
-  `big_text` varchar(100) NOT NULL,
-  `small_text_cn` varchar(200) CHARACTER SET gbk NOT NULL,
-  `big_text_cn` varchar(100) CHARACTER SET gbk NOT NULL,
-  `is_deleted` varchar(1) NOT NULL DEFAULT 'N',
-  `created_by` varchar(20) NOT NULL,
-  `updated_by` varchar(20) NOT NULL,
-  `created_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `main_header`
---
-
-INSERT INTO `main_header` (`id`, `small_text`, `big_text`, `small_text_cn`, `big_text_cn`, `is_deleted`, `created_by`, `updated_by`, `created_timestamp`, `updated_timestamp`) VALUES
-(0000000003, 'test', 'Test', '只是看我们最近的工程项目', '只是看我们最近的工程项目', 'N', 'test', 'test', '2017-12-12 13:59:41', '2017-12-12 13:59:41');
 
 -- --------------------------------------------------------
 
@@ -95,12 +68,21 @@ CREATE TABLE `user` (
   `password` varchar(100) NOT NULL,
   `last_successful` timestamp NULL DEFAULT NULL,
   `last_failed` timestamp NULL DEFAULT NULL,
+  `failed_attemps` int(1) NOT NULL DEFAULT '0',
+  `is_disabled` varchar(1) NOT NULL DEFAULT 'N',
   `is_deleted` varchar(1) NOT NULL DEFAULT 'N',
-  `created_by` varchar(20) NOT NULL,
-  `updated_by` varchar(20) NOT NULL,
+  `created_by` varchar(20) NOT NULL DEFAULT 'SYSTEM',
+  `updated_by` varchar(20) NOT NULL DEFAULT 'SYSTEM',
   `created_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `last_successful`, `last_failed`, `failed_attemps`, `is_disabled`, `is_deleted`, `created_by`, `updated_by`, `created_timestamp`, `updated_timestamp`) VALUES
+(0000000001, 'thopo', '4dfe52b98ed69dd92edaca4846c34ada', '2017-12-26 14:13:30', '2017-12-25 16:29:41', 0, 'N', 'N', 'SYSTEM', '', '2017-12-23 17:21:31', '2017-12-26 14:13:30');
 
 --
 -- Indexes for dumped tables
@@ -117,12 +99,6 @@ ALTER TABLE `chip_code`
 -- Indexes for table `gallery`
 --
 ALTER TABLE `gallery`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `main_header`
---
-ALTER TABLE `main_header`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -147,15 +123,10 @@ ALTER TABLE `chip_code`
 ALTER TABLE `gallery`
   MODIFY `id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `main_header`
---
-ALTER TABLE `main_header`
-  MODIFY `id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
